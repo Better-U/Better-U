8f54486813b9a5b8c195723d1ed006bd4ebf8398
Eric <ericso0303@gmail.com>
2016-04-27 20:30:36 -0700

    [Bugfix] Conform to standard

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var app = express();
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var db = require('./db.js');
-var dotenv = require('dotenv');
-dotenv.config();
+var express = require('express')
+var app = express()
+var cors = require('cors')
+var bodyParser = require('body-parser')
+// var db = require('./db.js')
+var dotenv = require('dotenv')
+dotenv.config()
 
-var health = require('./routes/health');
-var fitness = require('./routes/fitness');
-var challenge = require('./routes/challenge');
-var landing = require('./routes/landing');
-var users = require('./routes/users');
+var health = require('./routes/health')
+var fitness = require('./routes/fitness')
+var challenge = require('./routes/challenge')
+var landing = require('./routes/landing')
+var users = require('./routes/users')
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true, limit: '25mb'}));
-app.use(bodyParser.json({limit: '25mb', extended:true}));
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true, limit: '25mb'}))
+app.use(bodyParser.json({limit: '25mb', extended: true}))
 
-app.use('/api/users/', users);
-app.use('/api/landing/', landing);
-app.use('/api/health/', health);
-app.use('/api/fitness/', fitness);
-app.use('/api/challenge/', challenge);
+app.use('/api/users/', users)
+app.use('/api/landing/', landing)
+app.use('/api/health/', health)
+app.use('/api/fitness/', fitness)
+app.use('/api/challenge/', challenge)
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080
 
-app.listen(port, console.log('Magic happens on port', port));
+app.listen(port, console.log('Magic happens on port', port))
