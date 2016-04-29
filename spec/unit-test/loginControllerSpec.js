850a069c3648007fe3baf5e34cac2633e96eac3e
Jane Fong <janeefongg@gmail.com>
2016-04-29 07:42:01 -0700

    [Bugfix] Database schema

---
+++ spec/unit-test/loginControllerSpec.js
@@ -0,0 +1,25 @@
+describe('LoginCtrl', function () {
+  var $scope, $rootScope, createController, $httpBackend
+
+  // using angular mocks, we can inject the injector
+  // to retrieve our dependencies
+  beforeEach(module('myApp'))
+  beforeEach(inject(function($injector) {
+
+    // mock out our dependencies
+    $rootScope = $injector.get('$rootScope')
+    $httpBackend = $injector.get('$httpBackend')
+    $scope = $rootScope.$new()
+
+    createController = function () {
+      return $controller('LoginCtrl', {
+        $scope: $scope
+      })
+    }
+  }))
+
+  it('it should have an object used for storage', function() {
+    createController()
+    expect($scope.data).to.be.an('object')
+  })
+})
