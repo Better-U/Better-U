827f5abcb8df92e1f62ff20009c63a3bbbee32f1
Eric <ericso0303@gmail.com>
2016-04-29 09:49:44 -0700

    [Feature] signup signin queries

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,13 +1,40 @@
 var express = require('express')
 var router = express.Router()
-// var db = require('../db.js')
+var db = require('../db.js')
 var app = express()
 var bodyParser = require('body-parser')
+var bcrypt = require('bcrypt')
+var User = require('../helpers/user')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+var saltRounds = 10
 router.post('/', function (req, res) {
   // req.body.username and req.body.password hold info
-  res.send(req.body)
+
+  User.findUser(req.body.username)
+  .then(function(data){
+  	if (data.length === 0){
+  		User.hashPassword(req.body.password)
+  		.then(function(hash){
+  			User.insertUserPw(req.body.username, hash)
+  			  .then(function(){
+  			  	res.send('user added')
+  			  })
+
+  		})
+  	} else {
+  		res.send('You have an account')
+  	}
+  })
+
 })
+
+
+
+
+ 
+
+
+
 module.exports = router
