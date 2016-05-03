0a132171951170827cd13484315917f3cbfe3b08
Jane Fong <janeefongg@gmail.com>
2016-05-03 16:28:55 -0700

    [Feat] Token authorize server-side

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,9 +1,10 @@
 angular.module('myApp.dashboard', [])
 
-  .controller('DashboardCtrl', function ($scope, $state, authFactory) {
+  .controller('DashboardCtrl', function ($scope, $state, authFactory, $cookies) {
     $scope.signout = function () {
-      window.localStorage.removeItem('token')
       window.localStorage.removeItem('username')
+      $cookies.remove('token')
+      $cookies.remove('username')
       $state.go('landing')
     }
   })
