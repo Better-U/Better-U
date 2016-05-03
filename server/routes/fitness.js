0049b679e124383fc934d02af9bd07b3bb78fdef
Eirik <eiriklin@yahoo.com>
2016-05-03 14:53:01 -0700

    [Feat] Strength Forms working

--- server/routes/fitness.js
+++ server/routes/fitness.js
@@ -7,6 +7,46 @@ var User = require('../helpers/user')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+router.post('/strengthForm', function (req, res) {
+  var user = req.body.username
+  User.findUser(user)
+    .then(function (data) {
+      var strengthForm = [{
+        user_id: data[0].id,
+        date: req.body.date,
+        type: req.body.type,
+        sets: req.body.sets,
+        intensity: req.body.intensity,
+        duration: req.body.duration,
+        weight: req.body.weight,
+        reps: req.body.reps
+      }]
+      db.insert(strengthForm).into('strength_record').select('user_id', 'date', 'type', 'sets', 'intensity', 'duration', 'weight', 'reps')
+      .then(function (success) {
+        if (success) {
+          res.status(201).json({success: true})
+        } else {
+          res.status(404).json({success: false})
+        }
+      })
+    })
+})
+
+router.post('/getStrength', function (req, res) {
+  var user = req.body.username
+  User.findUser(user)
+    .then(function (data) {
+      db.select('*').from('strength_record').where({'strength_record.user_id': data[0].id})
+        .then(function (success) {
+          if (success) {
+            res.status(201).send(success)
+          } else {
+            res.status(404).json({success: false})
+          }
+        })
+    })
+})
+
 router.post('/cardioForm', function (req, res) {
   var user = req.body.username
   User.findUser(user)
@@ -19,16 +59,16 @@ router.post('/cardioForm', function (req, res) {
         duration: req.body.duration,
         pace: req.body.pace,
         intensity: req.body.intensity
-        }]
+      }]
       db.insert(cardioForm).into('cardio_record').select('user_id', 'date', 'type', 'distance', 'duration', 'pace', 'intensity')
-    .then(function (success) {
-      if(success) {
-        res.status(201).json({success: true})
-      } else {
-        res.status(404).json({success: false})
-      }
+      .then(function (success) {
+        if (success) {
+          res.status(201).json({success: true})
+        } else {
+          res.status(404).json({success: false})
+        }
+      })
     })
-  })
 })
 
 router.post('/getCardio', function (req, res) {
@@ -37,7 +77,7 @@ router.post('/getCardio', function (req, res) {
     .then(function (data) {
       db.select('*').from('cardio_record').where({'cardio_record.user_id': data[0].id})
         .then(function (success) {
-          if(success) {
+          if (success) {
             res.status(201).send(success)
           } else {
             res.status(404).json({success: false})
--- server/routes/health.js
+++ server/routes/health.js
@@ -2,4 +2,6 @@ var express = require('express')
 var router = express.Router()
 // var db = require('../db.js')
 
+
 module.exports = router
+
