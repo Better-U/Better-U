0d2dd9810e01b9355d02fff7d9ba62a42c26507e
Jessica Chou <j4chou@gmail.com>
2016-05-03 12:00:39 -0700

    [Feat] Profile Page Complete

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -8,19 +8,25 @@ angular.module('myApp.profile', ['factories'])
       .then(function (data) {
         console.log('data inside profileFactory.getProfile =', data.data[0])
         $scope.display = data.data[0]
+        $scope.prof = {activity: data.data[0].activitylvl,
+                       gym: data.data[0].gym
+                      }
+        console.log('scope interest', $scope.display.interest)
+        $scope.prof.interest = $scope.display.interest
       })
     }
     $scope.submitProfile = function () {
       console.log('submitted')
       console.log('localStorage.userId =', $window.localStorage.userId)
-      profileFactory.submitProfile($window.localStorage.userId, $scope.prof.bodyFat, $('input[name="prof.activity"]:checked').val(), $scope.prof.interest)
+      profileFactory.submitProfile($window.localStorage.userId, $scope.prof.weight, $scope.prof.bodyFat, $('input[name="prof.activity"]:checked').val(), $scope.prof.interest, $scope.prof.gym)
         .then(function (data) {
           console.log('profile data inside profile.js =', data)
         })
     }
 
     $scope.getGender = function (integer) {
-      if (integer === 0) {
+      var gender = Number(integer)
+      if (gender === 0) {
         return 'Male'
       } else {
         return 'Female'
