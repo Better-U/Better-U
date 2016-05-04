3961e7103cf54e36a5e3a1646ecfd6cad8e79c94
Eirik <eiriklin@yahoo.com>
2016-05-04 16:09:27 -0700

    [Fix] Fixing Strength display

--- client/app/fitness/strength.js
+++ client/app/fitness/strength.js
@@ -1,30 +1,26 @@
 angular.module('myApp.strength', ['factories'])
   .controller('StrengthCtrl', function ($scope, authFactory, strengthFactory, $cookies, profileFactory) {
     // console.log('this is username: ',authFactory.userData.username)
-    $scope.weight = 155
-    $scope.feet = 5
-    $scope.inches = 2
-    $scope.height_in = 61
-    $scope.bmi2
-    $scope.bodyfat = 12
     $scope.strengthList = []
+    $scope.user = $cookies.get('username')
 
-    profileFactory.getProfile()
-
-    var user = $cookies.get('username')
-    $scope.getStrength = function () {
-      strengthFactory.getStrength(user)
+    $scope.getStrengthProfile = function () {
+      profileFactory.getProfile($scope.user)
         .then(function (data) {
-          console.log(data)
-          $scope.strengthList = data.data
+          $scope.display = data.data[0]
         })
+        .then(
+          strengthFactory.getStrength($scope.user)
+            .then(function (data) {
+              $scope.strengthList = data.data
+            })
+        )
     }
-
-    $scope.getStrength()
+    $scope.getStrengthProfile()
 
     $scope.submitStrength = function () {
       strengthFactory.submitStrength(
-        user,
+        $scope.user,
         $scope.str.date,
         $scope.str.type,
         $scope.str.sets,
@@ -35,13 +31,15 @@ angular.module('myApp.strength', ['factories'])
       )
         .then(function (data) {
           $scope.str = {}
-          $scope.getStrength()
+          $scope.getStrengthProfile()
         })
     }
 
-    $scope.convert_height_in = function (feet, inches) {
-      $scope.height_in = (feet * 12) + inches
-      return $scope.height_in
+    $scope.convert_feet = function (inches) {
+      var feet = Math.floor(inches / 12)
+      var inch = inches - feet * 12
+      var tall = feet + '\'' + inch
+      return tall
     }
 
     $scope.onerepmax = function (weight, reps) {
@@ -52,25 +50,26 @@ angular.module('myApp.strength', ['factories'])
       if (weight_lb > 0 && height_in > 0) {
         var bmi_result = (weight_lb / (height_in * height_in)) * 703.06957964
       }
-      $scope.bmi2 = bmi_result
       return bmi_result.toFixed(2)
     }
 
     $scope.categorize = function (height, weight) {
       var bmi2 = $scope.bmi(height, weight)
       var category
-      if (bmi2 > 30) {
+      if (bmi2 >= 30) {
         category = 'You got work to do!'
       }
-      else if (bmi2 > 25 && bmi2 < 29.99) {
+      else if (bmi2 >= 25 && bmi2 < 29.99) {
         category = 'Time to exercise and eat right'
       }
-      else if (bmi2 > 18.5 && bmi2 < 24.99) {
-        category = "Looking good! Let's stay that way"
+      else if (bmi2 >= 18.5 && bmi2 < 24.99) {
+        category = 'Looking good! Lets stay that way'
       }
       else if (bmi2 < 18.5) {
         category = 'Bulk up!'
       }
       return category
     }
+
+
   })
