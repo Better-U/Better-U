d5083c45f70f1860f6a69cd1557cb4aec851143d
Michelle <mtham101@gmail.com>
2016-05-04 18:03:06 -0700

    [Feat] Render cardio burn on dashboard

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -5,7 +5,7 @@ angular.module('myApp.dashboard', [])
     console.log('this is profile data: ', profileFactory.getProfile(user))
 
     $scope.goalsData = null
-    $scope.display = null
+    $scope.dash = null
 
     $scope.signout = function () {
       $cookies.remove('token')
@@ -16,15 +16,43 @@ angular.module('myApp.dashboard', [])
     $scope.getDashboardProfile = function () {
       profileFactory.getProfile(user)
         .then(function (data) {
-          $scope.display = data.data
-          console.log('this is getProfile data: ', data.data)
+          $scope.dash = data.data[0]
+          console.log('this is getProfile data: ', data.data[0].activitylvl)
         })
     }
     $scope.getDashboardProfile()
 
-    // $scope.calorieBurn = function (gender, age, weight, height, activity) {
-
-    // }
+    $scope.calorieBurn = function (gender, age, weight, height, activity) {
+      var numAge = Number(age)
+      var numWeight = Number(weight)
+      var numHeight = Number(height)
+      var total
+      if (gender == 0) {
+        var maleBMR = 66.5 + (6.23 * weight) + (12.7 * height) - (6.8 * age)
+        if (activity === 'sendentary') {
+          total = (maleBMR * 1.2).toFixed(2)
+        } else if (activity === 'light') {
+          total = (maleBMR * 1.375).toFixed(2)
+        } else if (activity === 'moderate') {
+          total = (maleBMR * 1.55).toFixed(2)
+        } else if (activity === 'active') {
+          total = (maleMBR * 1.725).toFixed(2)
+        }
+      } else if (gender == 1) {
+        var femaleBMR = 655.1 + (4.35 * numWeight) + (4.7 * numHeight) - (4.7 * numAge)
+        console.log('this is femaleBMR: ', femaleBMR)
+        if (activity === 'sendentary') {
+          total = (femaleBMR * 1.2).toFixed(2)
+        } else if (activity === 'light') {
+          total = (femaleBMR * 1.375).toFixed(2)
+        } else if (activity === 'moderate') {
+          total = (femaleBMR * 1.55).toFixed(2)
+        } else if (activity === 'active') {
+          total = (femaleBMR * 1.725).toFixed(2)
+        }
+      }
+      return total
+    }
 
     $scope.removeLog = function (id) {
       console.log(id)
