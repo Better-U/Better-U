2565c9a05b46a1b227d53b22679dd63be38b64bb
Michelle <mtham101@gmail.com>
2016-05-04 19:25:18 -0700

    [Feat] Implement calculators on dashboard

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -2,7 +2,6 @@ angular.module('myApp.dashboard', [])
 
   .controller('DashboardCtrl', function ($scope, $state, GoalsFactory, $cookies, profileFactory) {
     var user = $cookies.get('username')
-    console.log('this is profile data: ', profileFactory.getProfile(user))
 
     $scope.goalsData = null
     $scope.dash = null
@@ -17,10 +16,36 @@ angular.module('myApp.dashboard', [])
       profileFactory.getProfile(user)
         .then(function (data) {
           $scope.dash = data.data[0]
-          console.log('this is getProfile data: ', data.data[0].activitylvl)
         })
     }
     $scope.getDashboardProfile()
+
+    $scope.userBMI = function (height, weight) {
+      var bmiHeight = Number(height)
+      var bmiWeight = Number(weight)
+      if (weight> 0 && height > 0) {
+        var BMI = ((weight / (height * height)) * 703.06957964).toFixed(2)
+      }
+      return BMI
+    }
+
+    $scope.categorize = function (height, weight) {
+      var bmi2 = $scope.userBMI(height, weight)
+      var category
+      if (bmi2 >= 30) {
+        category = 'You got work to do!'
+      }
+      else if (bmi2 >= 25 && bmi2 < 29.99) {
+        category = 'Time to exercise and eat right'
+      }
+      else if (bmi2 >= 18.5 && bmi2 < 24.99) {
+        category = 'Looking good! Lets stay that way'
+      }
+      else if (bmi2 < 18.5) {
+        category = 'Bulk up!'
+      }
+      return category
+    }
 
     $scope.calorieBurn = function (gender, age, weight, height, activity) {
       var numAge = Number(age)
@@ -28,26 +53,26 @@ angular.module('myApp.dashboard', [])
       var numHeight = Number(height)
       var total
       if (gender == 0) {
-        var maleBMR = 66.5 + (6.23 * weight) + (12.7 * height) - (6.8 * age)
+        var maleBMR = 66.5 + (6.23 * numWeight) + (12.7 * numHeight) - (6.8 * numAge)
         if (activity === 'sendentary') {
-          total = (maleBMR * 1.2).toFixed(2)
+          total = (maleBMR * 1.2).toFixed()
         } else if (activity === 'light') {
-          total = (maleBMR * 1.375).toFixed(2)
+          total = (maleBMR * 1.375).toFixed()
         } else if (activity === 'moderate') {
-          total = (maleBMR * 1.55).toFixed(2)
+          total = (maleBMR * 1.55).toFixed()
         } else if (activity === 'active') {
-          total = (maleMBR * 1.725).toFixed(2)
+          total = (maleMBR * 1.725).toFixed()
         }
       } else if (gender == 1) {
         var femaleBMR = 655.1 + (4.35 * numWeight) + (4.7 * numHeight) - (4.7 * numAge)
         if (activity === 'sendentary') {
-          total = (femaleBMR * 1.2).toFixed(2)
+          total = (femaleBMR * 1.2).toFixed()
         } else if (activity === 'light') {
-          total = (femaleBMR * 1.375).toFixed(2)
+          total = (femaleBMR * 1.375).toFixed()
         } else if (activity === 'moderate') {
-          total = (femaleBMR * 1.55).toFixed(2)
+          total = (femaleBMR * 1.55).toFixed()
         } else if (activity === 'active') {
-          total = (femaleBMR * 1.725).toFixed(2)
+          total = (femaleBMR * 1.725).toFixed()
         }
       }
       return total
@@ -56,7 +81,14 @@ angular.module('myApp.dashboard', [])
     $scope.userBMR = function (gender, age, weight, height) {
       var uAge = Number(age)
       var uWeight = Number(weight)
-      var uRight = Number()
+      var uHeight = Number(height)
+      var BMR
+      if (gender == 0) {
+        BMR = (66.5 + (6.23 * uWeight) + (12.7 * uHeight) - (6.8 * uAge)).toFixed(2)
+      } else if (gender == 1) {
+        BMR = (655.1 + (4.35 * uWeight) + (4.7 * uHeight) - (4.7 * uAge)).toFixed(2)
+      }
+      return BMR
     }
 
     $scope.removeLog = function (id) {
@@ -85,7 +117,7 @@ angular.module('myApp.dashboard', [])
 
       return days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
     }
-    
+
     $scope.goalPercentage = function (current, max) {
       return current/max * 100
     }
