7c5aa94bbd1157b173140a34b8d479644fb22789
Jane Fong <janeefongg@gmail.com>
2016-05-05 19:39:58 -0700

    [Feat] Dashboard/Goal css

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -10,40 +10,19 @@ angular.module('myApp.landing', [])
     $scope.animationsEnabled = true
 
     $scope.signup = function () {
-      var modalInstance = $uibModal.open({
+      $uibModal.open({
         animation: $scope.animationsEnabled,
         templateUrl: 'app/signup/signup.html',
-        controller: 'SignupCtrl',
-        resolve: {
-          items: function () {
-            return $scope.items
-          }
-        }
+        controller: 'SignupCtrl'
       })
 
-      modalInstance.result.then(function (selectedItem) {
-        $scope.selected = selectedItem
-      }, function () {
-        console.log('hi')
-      })
     }
 
     $scope.signin = function () {
-      var modalInstance = $uibModal.open({
+      $uibModal.open({
         animation: $scope.animationsEnabled,
         templateUrl: 'app/signin/signin.html',
-        controller: 'SigninCtrl',
-        resolve: {
-          items: function () {
-            return $scope.items
-          }
-        }
-      })
-
-      modalInstance.result.then(function (selectedItem) {
-        $scope.selected = selectedItem
-      }, function () {
-        console.log('hi')
+        controller: 'SigninCtrl'
       })
     }
   })
