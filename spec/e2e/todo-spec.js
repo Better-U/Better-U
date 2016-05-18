b7e2cba87fe82cbfa904a431a49ac2700bdc6427
Eric <ericso0303@gmail.com>
2016-05-17 17:41:36 -0700

    [Testing] Protractor test unfinished

--- spec/e2e/todo-spec.js
+++ spec/e2e/todo-spec.js
@@ -1,3 +1,5 @@
+// var webdriver = require('selenium-webdriver');
+
 describe('Protractor Demo App', function () {
   var signUpButton = element(by.id('landingSignUpButton'))
   var usernameInput = element(by.id('usernameSignup'))
@@ -8,38 +10,50 @@ describe('Protractor Demo App', function () {
   var height = element(by.id('heightSignup'))
   var weight = element(by.id('weightSignup'))
   var gender = element(by.className('genderSignupMale'))
-  var activity = element(by.id('activitySignupSedentary'))
-  var interest = element(by.id('interestSignupCardio'))
-  var gym = element(by.id('gymSignup24'))
+  var activity = element(by.className('activitySignupSedentary'))
+  var interest = element(by.className('interestSignupCardio'))
+  var gym = element(by.className('gymSignup24'))
   var submitProfile = element(by.id('registerProfileSubmit'))
-  var goProfile = element(by.className('profileNavigator'))
-  var profileUsername = element(by.id('profileUsername'))
+
+
+  // var goProfile = element(by.className('profileNavigator'))
+  // var profileUsername = element(by.id('profileUsername'))
   // beforeEach(function () {
   //   browser.get('http://juliemr.github.io/protractor-demo/')
   // })
-  // beforeEach(function(){
-  //   browser.get('http://localhost:8080/#/');
-  // })
+  beforeEach(function(){
+    browser.get('http://localhost:8080/#/');
+  })
 
   //for repeater
   // var phoneList = element.all(by.repeater('phone in phones'));
   //for model
   //var query = element(by.model('query'));
-  it('should be able to signup', function () {
-    browser.get('http://localhost:8080/#/');
+  it('should have a title of BetterU - Fitness', function () {
+  expect(browser.getTitle()).toEqual('BetterU - Fitness');
+  })
+  it('should be able to sign up', function(){
+    // browser.get('http://localhost:8080/#/');
     signUpButton.click()
-    usernameInput.sendKeys('cuteElephant')
-    passwordInput.sendKeys('cuteElephant')
+    usernameInput.sendKeys('cuteElephant12')
+    passwordInput.sendKeys('cuteElephant12')
     submitSignup.click()
     age.sendKeys(50)
     height.sendKeys(70)
+    weight.sendKeys(170)
     gender.click()
     activity.click()
+    interest.click()
     gym.click()
     submitProfile.click()
-    goProfile.click()
-    expect(profileUsername).toEqual('Name: ')
+    browser.waitForAngular()
+        var welcomed = element(by.id('welcomeUser'))
+    expect(welcomed.getText()).toEqual('Welcome back, cuteElephant12!')
+    // webdriver.wait(expect(welcomed.getText()).toEqual('Welcome back, cuteElephant7!'), 5000)
+    
 
+    // goProfile.click()
+    // expect(profileUsername).toEqual('Name: ')
   })
 
   // it('should add one and two', function () {
